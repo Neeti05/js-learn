262fa244f8c276ff140fb4a8a76f8674a77eaf4c
Neeti05 <155546757+Neeti05@users.noreply.github.com>
2025-04-25 22:51:59 +0000

    yay

--- 01_basics/05_datatypes.js
+++ 01_basics/05_datatypes.js
@@ -1,15 +1,34 @@
-// primitive - String, Number, Boolean, Null, Undefined, Symbol, BigInt
-const id = Symbol('123')
-const anotherid = Symbol('123')
-console.log(id == anotherid)
+// // primitive - String, Number, Boolean, Null, Undefined, Symbol, BigInt
+// const id = Symbol('123')
+// const anotherid = Symbol('123')
+// console.log(id == anotherid)
 
-// non-primitive (reference) - array, objects, functions
+// // non-primitive (reference) - array, objects, functions
 
-const heros = ["shaktiman", "batman","naagraj"]
-let myObj= {
-    name:"neeti",
-    age:20
+// const heros = ["shaktiman", "batman","naagraj"]
+// let myObj= {
+//     name:"neeti",
+//     age:20
+// }
+// const MyFunction = function(){
+// console.log("Hello World")
+// }
+
+
+//MEMORY
+//Stack(Primitive)-copy, Heap(Reference)-reference
+let name= "neeti"
+let anothername = name
+anothername = "neo"
+console.log(name)
+console.log(anothername)
+
+let userOne = {
+    email : "user@gmail.com",
+    upi : "user@ybl"
 }
-const MyFunction = function(){
-console.log("Hello World")
-}
+
+let userTwo = userOne
+userTwo.email = "user2@gmail.com"
+console.log(userOne)
+console.log(userTwo)

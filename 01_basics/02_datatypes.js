e906de22cfee31bc543c060a74363feba2f4f3e7
Neeti05 <155546757+Neeti05@users.noreply.github.com>
2025-04-25 13:07:01 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS code as newer version
+// Code readability should be high
+//string
+let name = "Neeti"
+// number(2 power 53) or bigInt
+let age = 20
+//boolean
+let IsLogged = false  
+// null - standalone value
+// undefined - no value defined
+// symbol - uniqueness
+
+console.log(typeof "Neeti")
+console.log(typeof age)
+
+console.log(typeof null) // output is object
+console.log(typeof undefined) // output is undefined

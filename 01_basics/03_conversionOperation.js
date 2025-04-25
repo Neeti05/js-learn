47b0c406597e4d4f16a4e7a58f28234dfc643930
Neeti05 <155546757+Neeti05@users.noreply.github.com>
2025-04-25 22:13:22 +0000

    Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,24 +1,41 @@
-let score = "33"
+// let score = "33"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
-/*
-"33" - 33
-"33abc" - NaN
-true - 1
-false - 0 
-*/
+// /*
+// "33" - 33
+// "33abc" - NaN
+// true - 1
+// false - 0 
+// */
 
-let islogged =1;
+// let islogged =1;
 
-let booleanislogged = Boolean(islogged);
-console.log(booleanislogged);
+// let booleanislogged = Boolean(islogged);
+// console.log(booleanislogged);
 
 //1-true,0-false
 //""-false, "value"-true
+
+// let value = 3;
+// let negvalue = - value;
+// console.log(negvalue)
+
+// let str1 = "hello"
+// let str2 = " hitesh"
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log(1+"2")
+// console.log("1"+2)
+// console.log("1"+2+1)
+// console.log(1+2+"1")
+
+
+

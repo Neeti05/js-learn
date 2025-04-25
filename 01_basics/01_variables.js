9b64c7f4ad55416a213653e016cf787304fea9f7
Neeti05 <155546757+Neeti05@users.noreply.github.com>
2025-04-25 12:48:44 +0000

    01 changed

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const accountID = 144553 //preferred
+let accountEmail = "neeti@gmail.com" //preferred
+var accountPass = "12345" // not preferred
+accountCity = "Jaipur" // not preferred
+let accountState; //undefined

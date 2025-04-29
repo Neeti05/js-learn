11a38b1b1e6c323bf1e0a6415ba8638481818420
Neeti05 <155546757+Neeti05@users.noreply.github.com>
2025-04-29 11:30:47 +0000

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,23 @@
+// const name ="Neeti"
+// const count = 50
+
+// console.log(`Hello my name is ${name} and my count is ${count}`)
+
+const gameName = new String ('Neetiii-Donutttt-Coffeee')
+// console.log(gameName.__proto__)
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(3))
+// console.log(gameName.indexOf('t'))
+
+const newStr = gameName.substring(0,4)
+console.log(newStr)
+
+const anoStr = gameName.slice (-7,4)
+console.log(anoStr)
+
+const newString = "     NEETi     "
+console.log(newString)
+console.log(newString.trim())
+
+console.log(gameName.split('-'))

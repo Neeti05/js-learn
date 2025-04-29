b2d9eb8425fe6c0e3b6f01281e331ca8ac483252
Neeti05 <155546757+Neeti05@users.noreply.github.com>
2025-04-29 13:44:17 +0000

    date

---
+++ 01_basics/07_date.js
@@ -0,0 +1,9 @@
+let myDate = new Date ();
+console.log(myDate.toString());
+// date is an object 
+
+const CreateDate = new Date (2023,0,23)
+console.log(CreateDate.toString());
+
+let Time = Date.now();
+console.log(Time)
